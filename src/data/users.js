c0e29aa234485dd6d30f4d17906759f0b4c10c62
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-24 14:47:29 -0700

    commit

--- src/data/users.js
+++ src/data/users.js
@@ -5,6 +5,7 @@
       lastName: "Capasso",
       email: "kate@gmail.com",
       password: "1301726F75E647F934EE226B03D69C70",
+      createdOn: 1593035130335,
    },
    {
       id: "62f8f43a-6584-45c0-b8b7-320140ebf169",
@@ -12,6 +13,7 @@
       lastName: "Smith",
       email: "sally@gmail.com",
       password: "6C8E3B2514447CEE0A598F3DFBD1704D",
+      createdOn: 1593035139832,
    },
    {
       id: "8d7dce3e-91ca-4e28-a8fc-c966ad770f29",
@@ -19,5 +21,6 @@
       lastName: "McDaniel",
       email: "brandon@gmail.com",
       password: "4BD3F018D9A8B9C51976EC8FF6344C66",
+      createdOn: 1593035148640,
    },
 ];
