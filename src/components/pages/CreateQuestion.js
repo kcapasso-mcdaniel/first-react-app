4911fad394f455376b87658cd316c399e7e31dc7
kcapasso-mcdaniel <64052620+kcapasso-mcdaniel@users.noreply.github.com>
2020-06-30 12:42:14 -0700

    update CreateQuestion page

--- src/components/pages/CreateQuestion.js
+++ src/components/pages/CreateQuestion.js
@@ -1,43 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../ui/Navigation";
+import Answer from "../ui/Answer";
+import Question from "../ui/Question";
 
 export default class CreateQuestion extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         answerInputs: [],
+         addAnswerInput: [],
          isAnswerDisplayed: true,
       };
    }
 
    // function renders a new answer input on the page
    addNewAnswer(i) {
-      const answerInputs = this.state.answerInputs;
-      answerInputs.push(i);
-      this.setState({ answerInputs: answerInputs });
-   }
-
-   createTheAnswer() {
-      return (
-         <>
-            <div className={"form-group row"}>
-               <label htmlFor="answer" className="col-sm-2 col-form-label ml-2">
-                  Answer
-               </label>
-               <div className="col-sm-10">
-                  <input type="text" className="form-control" id="answer" />
-               </div>
-               <button
-                  className="btn btn-warning"
-                  type="button"
-                  id="delete-answer"
-               >
-                  Delete
-               </button>
-            </div>
-         </>
-      );
+      const addAnswerInput = [...this.state.addAnswerInput];
+      addAnswerInput.push(i);
+      this.setState({ addAnswerInput: addAnswerInput });
    }
 
    render() {
@@ -46,7 +26,7 @@ export default class CreateQuestion extends React.Component {
             <div className="row">
                <div className="col-12">
                   <Navigation />
-                  <div className="form-group">
+                  <div className="form-group" key={Question.id}>
                      <label htmlFor="formGroupExampleInput">Question</label>
                      <input
                         type="text"
@@ -73,16 +53,14 @@ export default class CreateQuestion extends React.Component {
                         className="btn btn-warning"
                         type="button"
                         id="delete-answer"
-                        onClick={() => {
-                           this.deleteAnswer();
-                        }}
                      >
                         Delete
                      </button>
                   </div> */}
-
                   {/* state to add an answer input when add answer button is clicked */}
-                  {this.state.answerInputs.map(this.createTheAnswer)}
+                  {this.state.addAnswerInput.map((answer, i) => (
+                     <Answer key={`answer-${i}`} index={i} />
+                  ))}
                   <button
                      type="buttons"
                      className="btn-success btn-lg ml-3"
---
+++ src/components/ui/Answer.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default class Answer extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         isAnswerShowing: true,
+      };
+   }
+
+   // delete the input on the page
+   deleteAnswer() {
+      this.setState({ isAnswerShowing: !this.state.isAnswerShowing });
+   }
+   render() {
+      return (
+         <>
+            {this.state.isAnswerShowing && (
+               <div className="form-group row">
+                  <label
+                     htmlFor="answer"
+                     className="col-sm-2 col-form-label ml-2"
+                  >
+                     Answer
+                  </label>
+                  <div className="col-sm-10">
+                     <input type="text" className="form-control" id="answer" />
+                  </div>
+
+                  <button
+                     className="btn btn-warning"
+                     type="button"
+                     id="delete-answer"
+                     onClick={() => this.deleteAnswer(this.props.index)}
+                  >
+                     Delete
+                  </button>
+               </div>
+            )}
+         </>
+      );
+   }
+}
